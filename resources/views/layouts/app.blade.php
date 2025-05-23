6315b837208c5844e942416f91af832ad0bcd9f4
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-05-23 19:50:31 +0600

    sinup and login page done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') | Car Selling Website</title>
-</head>
-
-<body>
-    <header>
-        <h1>Header Section</h1>
-    </header>
+@extends('layouts.clean')
+@section('childContent')
+    @include('layouts.partials.header')
     @yield('main-content')
-    <footer>
-        <h1>Footer Section</h1>
-    </footer>
-</body>
-
-</html>
+    <footer></footer>
+@endsection
