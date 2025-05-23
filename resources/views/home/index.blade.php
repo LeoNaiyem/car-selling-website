ebf99ccd9d02ab61d30f5fcd08aa9b8261865c6f
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-05-23 16:53:55 +0600

    added basic layout

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'Home Page')
+@section('main-content')
+    <h1>Main content will be added here</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') | Car Selling Website</title>
+</head>
+
+<body>
+    <header>
+        <h1>Header Section</h1>
+    </header>
+    @yield('main-content')
+    <footer>
+        <h1>Footer Section</h1>
+    </footer>
+</body>
+
+</html>
